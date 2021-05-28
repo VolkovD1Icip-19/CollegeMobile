c28e1365633e1b744a1eabf5b42da488f012e497
kpk304kab <kpk304kab@outlook.com>
2021-05-28 13:32:50 +0500

    213

--- CollegeMobile/MainPage.xaml.cs
+++ CollegeMobile/MainPage.xaml.cs
@@ -14,5 +14,30 @@ namespace CollegeMobile
         {
             InitializeComponent();
         }
+
+        private void Serch_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Serch());
+        }
+
+        private void Home_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Filial_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Help_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Special_Tapped(object sender, EventArgs e)
+        {
+
+        }
     }
 }
---
+++ CollegeMobile/Serch.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CollegeMobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Serch : ContentPage
+    {
+        public Serch()
+        {
+            InitializeComponent();
+        }
+
+        private void SpecialArrow_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+
+        private void SpecialArrow1_Tapped(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
