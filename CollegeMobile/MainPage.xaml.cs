66aff06d7db38887bddd8a2bfd7bcd945f09024f
Haron Kukarachi <xaron.baron788@gmail.com>
2021-05-31 10:09:31 +0300

    1

--- CollegeMobile/MainPage.xaml.cs
+++ CollegeMobile/MainPage.xaml.cs
@@ -14,5 +14,13 @@ namespace CollegeMobile
         {
             InitializeComponent();
         }
+        private void Eko_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new SpecEKO());
+        }
+        private void BackSpec_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
     }
 }
