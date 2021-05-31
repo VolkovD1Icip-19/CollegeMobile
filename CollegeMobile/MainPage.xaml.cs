060d6d1ebaffba6dd355388d9ba03f423b7ca6df
вадим гаврилов <df_vadim_dfg@mail.ru>
2021-05-31 10:00:43 +0500

    123456

--- CollegeMobile/MainPage.xaml.cs
+++ CollegeMobile/MainPage.xaml.cs
@@ -29,5 +29,30 @@ namespace CollegeMobile
         {
             Navigation.PushAsync(new Gor());
         }
+
+        private void Special_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Serch_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Home_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Filial_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Help_Tapped(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- CollegeMobile/Plast.xaml.cs
+++ CollegeMobile/Plast.xaml.cs
@@ -16,5 +16,35 @@ namespace CollegeMobile
         {
             InitializeComponent();
         }
+
+        private void Special_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Serch_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Home_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Filial_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Help_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SpecialArrow1_Tapped(object sender, EventArgs e)
+        {
+
+        }
     }
 }
